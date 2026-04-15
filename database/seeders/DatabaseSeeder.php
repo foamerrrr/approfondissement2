aefd9a97ee925afcae235d381a8cfd201750c457
foamerrrr <laurentchhou3@gmail.com>
2026-04-15 14:45:25 +0200

    activite 8 etape 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,9 @@ use App\Models\User;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,19 +19,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $viewDashboardPermission = Permission::firstOrCreate(['name' => 'view dashboard']);
+        $adminRole->givePermissionTo($viewDashboardPermission);
+
        // Utilisateur "admin"
-        $admin = User::factory()->create([
-            'name'     => 'Admin',
+        $admin = User::updateOrCreate([
             'email'    => 'admin@example.com',
+        ], [
+            'name'     => 'Admin',
             'password' => bcrypt('password'),
         ]);
 
         // Utilisateur "user"
-        $user = User::factory()->create([
-            'name'     => 'User',
+        $user = User::updateOrCreate([
             'email'    => 'user@example.com',
+        ], [
+            'name'     => 'User',
             'password' => bcrypt('password'),
         ]);
+
+        $admin->assignRole($adminRole);
 
         // Produits de l'admin
         Product::factory()->count(3)->create([
